2eb915845e1ce33bc9407bd9d00f65a0809ba58e
kathryn smith <smithkb@uchicago.edu>
2017-12-08 17:11:37 -0600

    Finish Day 4

--- day4/day4.rb
+++ day4/day4.rb
@@ -12,7 +12,6 @@ data.map do |key, row|
   row.each_with_index do |value, index|
     [*index..(row.length - 1)].each do |place|
       compare = row[place+1]
-      puts "Comparing #{value} with #{compare}"
       if !compare.nil? && value == compare
         found_match = true
       end
@@ -26,11 +25,27 @@ puts "Answer: #{valid_keys}"
 
 puts "Solving Part 2..."
 
-data[0] = "abcde fghij".split(" ")
-data[1] = "abcde xyz ecdab".split(" ")
-data[2] = "a ab abc abd abf abj".split(" ")
-data[3] = "iiii oiii ooii oooi oooo".split(" ")
-data[4] = "oiii ioii iioi iiio".split(" ")
+# data[0] = "abcde fghij".split(" ")
+# data[1] = "abcde xyz ecdab".split(" ")
+# data[2] = "a ab abc abd abf abj".split(" ")
+# data[3] = "iiii oiii ooii oooi oooo".split(" ")
+# data[4] = "oiii ioii iioi iiio".split(" ")
 #Sample data answer is 3
 
+valid_keys = 0
+data.map do |key, row|
+  found_match = false
+  row.each_with_index do |value, index|
+    [*index..(row.length - 1)].each do |place|
+      compare = row[place+1]
+      puts "Comparing #{value.chars.sort} with #{compare.chars.sort}" if !compare.nil?
+      if !compare.nil? && value.chars.sort == compare.chars.sort
+        found_match = true
+      end
+    end
+  end
+  valid_keys += 1 if !found_match
+end
 
+puts "Valid keys: #{valid_keys}"
+#Answer 265
